1c5f25bbee4c89147e26a2cb133d98d0bcea151c
iliakakurin <49334425+iliakakurin@users.noreply.github.com>
2020-12-08 15:50:27 +0500

    Update 2d_array.py

--- 2d_array.py
+++ 2d_array.py
@@ -1,7 +1,12 @@
+# КОНСТАНТЫ
+TO_WIN = 77
+PLUS_MOVE = 1
+MULT_MOVE = 2
+START_FIRST = 7
 # создаем поле
-win = [] * 77
-for i in range(77):
-    win.append([0] * 77)
+win = [] * TO_WIN
+for i in range(TO_WIN):
+    win.append([0] * TO_WIN)
 for i in range(len(win)):
     for j in range(len(win)):
         if i == 0 or j == 0:
@@ -10,9 +15,9 @@ for i in range(len(win)):
 for i in range(1, len(win)):
     for j in range(1, len(win)):
         # если из положения (i,j) удвоение какой-либо кучи ведет к победе, ставим единицу
-        if i * 2 + j >= 77 and win[i][j] == 0: # заполняем очевидные выигрышные позиции
+        if i * 2 + j >= TO_WIN and win[i][j] == 0: # заполняем очевидные выигрышные позиции
             win[i][j] = 1
-        if i + j * 2 >= 77 and win[i][j] == 0: # заполняем очевидные выигрышные позиции
+        if i + j * 2 >= TO_WIN and win[i][j] == 0: # заполняем очевидные выигрышные позиции
             win[i][j] = 1
 
 #print(win)
@@ -21,22 +26,22 @@ for index in range(1):
         for j in range(len(win) - 1, 0, -1):
             # Если все ходы ведут в нечетные клетки, ход проигрышный - заполняем его четным числом
             # Количество ходов выбираем максимальное, чтобы затянуть игру
-            if win[i][j] == 0 and (win[i+1][j] % 2 == 1 and win[i*2][j] % 2 == 1 and win[i][j+1] % 2 == 1 and win[i][j*2] % 2 == 1):
-                win[i][j] = max(win[i+1][j], win[i*2][j], win[i][j+1], win[i][j*2]) + 1
+            if win[i][j] == 0 and (win[i+PLUS_MOVE][j] % 2 == 1 and win[i*MULT_MOVE][j] % 2 == 1 and win[i][j+PLUS_MOVE] % 2 == 1 and win[i][j*MULT_MOVE] % 2 == 1):
+                win[i][j] = max(win[i+PLUS_MOVE][j], win[i*MULT_MOVE][j], win[i][j+PLUS_MOVE], win[i][j*MULT_MOVE]) + 1
             # Если есть ход, который ведет в четную клетку, заполняем его нечетным числом
             # Количество ходов выбираем минимальное, чтобы быстрее закончить игру
-            elif win[i][j] == 0 and (win[i+1][j] % 2 == 0 or win[i*2][j] % 2 == 0 or win[i][j+1] % 2 == 0 or win[i][j*2] % 2 == 0) and (win[i+1][j] * win[i*2][j] * win[i][j+1] * win[i][j*2]) != 0:
+            elif win[i][j] == 0 and (win[i+PLUS_MOVE][j] % 2 == 0 or win[i*MULT_MOVE][j] % 2 == 0 or win[i][j+PLUS_MOVE] % 2 == 0 or win[i][j*MULT_MOVE] % 2 == 0) and (win[i+PLUS_MOVE][j] * win[i*MULT_MOVE][j] * win[i][j+PLUS_MOVE] * win[i][j*MULT_MOVE]) != 0:
                 min_steps = 10000
-                if win[i+1][j] < min_steps and win[i+1][j] % 2 == 0:
-                    min_steps = win[i+1][j]
-                if win[i*2][j] < min_steps and win[i*2][j] % 2 == 0:
-                    min_steps = win[i*2][j]
-                if win[i][j+1] < min_steps and win[i][j+1] % 2 == 0:
-                    min_steps = win[i][j+1]
-                if win[i][j*2] < min_steps and win[i][j*2] % 2 == 0:
-                    min_steps = win[i][j*2]
+                if win[i+PLUS_MOVE][j] < min_steps and win[i+PLUS_MOVE][j] % 2 == 0:
+                    min_steps = win[i+PLUS_MOVE][j]
+                if win[i*MULT_MOVE][j] < min_steps and win[i*MULT_MOVE][j] % 2 == 0:
+                    min_steps = win[i*MULT_MOVE][j]
+                if win[i][j+PLUS_MOVE] < min_steps and win[i][j+PLUS_MOVE] % 2 == 0:
+                    min_steps = win[i][j+PLUS_MOVE]
+                if win[i][j*MULT_MOVE] < min_steps and win[i][j*MULT_MOVE] % 2 == 0:
+                    min_steps = win[i][j*MULT_MOVE]
                 win[i][j] = min_steps + 1
-                
+
 # Выводим итоговый массив
 # print('____', end='')
 # for i in range(len(win)):
@@ -46,4 +51,4 @@ for index in range(1):
 #    print(i, win[i])
 
 # вывод только 7 камней в первой куче
-# print(win[7])
+print(win[START_FIRST])
